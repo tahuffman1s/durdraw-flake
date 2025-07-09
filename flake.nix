7c3a76944c310463e0eb684db93ec72a409b7e97
Travis Huffman <huffmantravis57@protonmail.com>
2025-07-09 09:50:34 -0500

    updating flake

--- flake.nix
+++ flake.nix
@@ -1,85 +1,82 @@
 {
   description = "Durdraw - ASCII, ANSI and Unicode art editor for UNIX terminals";
-  format = "setuptools";
-  pyproject = true;
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.${system};
-      in
-      {
-        packages = {
-          default = self.packages.${system}.durdraw;
-          
-          durdraw = pkgs.python3Packages.buildPythonApplication rec {
-            pname = "durdraw";
-            version = "0.29.0";
+    let
+      pkgs = nixpkgs.${system};
+    in
+    {
+      packages = {
+        default = self.packages.${system}.durdraw;
+        
+        durdraw = pkgs.python3Packages.buildPythonApplication rec {
+          pname = "durdraw";
+          version = "0.29.0";
 
-            src = pkgs.fetchFromGitHub {
-              owner = "cmang";
-              repo = "durdraw";
-              rev = version;
-              sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-            };
-
-            propagatedBuildInputs = [ ];
-
-            nativeBuildInputs = with pkgs.python3Packages; [
-              setuptools
-              wheel
-            ];
-
-            passthru.optional-dependencies = {
-              export = [ pkgs.ansilove ];
-              fetch = [ pkgs.neofetch ];
-            };
-
-            buildInputs = [ pkgs.ncurses ];
-
-            checkInputs = with pkgs.python3Packages; [ pytest ];
-
-            checkPhase = ''
-              runHook preCheck
-              pytest -vv test/
-              runHook postCheck
-            '';
-
-            meta = with pkgs.lib; {
-              description = "ASCII, ANSI and Unicode art editor for UNIX terminals";
-              homepage = "https://github.com/cmang/durdraw";
-              license = licenses.bsd3;
-              maintainers = with maintainers; [ Travis Huffman ];
-              platforms = platforms.unix;
-              mainProgram = "durdraw";
-            };
+          src = pkgs.fetchFromGitHub {
+            owner = "cmang";
+            repo = "durdraw";
+            rev = version;
+            sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           };
-        };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.durdraw;
-        };
+          propagatedBuildInputs = [ ];
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.durdraw ];
-          
-          packages = with pkgs; [
-            python3
-            python3Packages.pytest
-            # Optional tools for full functionality
-            ansilove
-            neofetch
+          nativeBuildInputs = with pkgs.python3Packages; [
+            setuptools
+            wheel
           ];
 
-          shellHook = ''
-            echo "Durdraw development environment"
-            echo "Run 'durdraw' to start the application"
-            echo "Optional: ansilove (PNG export), neofetch (system info)"
+          passthru.optional-dependencies = {
+            export = [ pkgs.ansilove ];
+            fetch = [ pkgs.neofetch ];
+          };
+
+          buildInputs = [ pkgs.ncurses ];
+
+          checkInputs = with pkgs.python3Packages; [ pytest ];
+
+          checkPhase = ''
+            runHook preCheck
+            pytest -vv test/
+            runHook postCheck
           '';
+
+          meta = with pkgs.lib; {
+            description = "ASCII, ANSI and Unicode art editor for UNIX terminals";
+            homepage = "https://github.com/cmang/durdraw";
+            license = licenses.bsd3;
+            maintainers = with maintainers; [ Travis Huffman ];
+            platforms = platforms.unix;
+            mainProgram = "durdraw";
+          };
         };
-      });
+      };
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = self.packages.${system}.durdraw;
+      };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.${system}.durdraw ];
+        
+        packages = with pkgs; [
+          python3
+          python3Packages.pytest
+          # Optional tools for full functionality
+          ansilove
+          neofetch
+        ];
+
+        shellHook = ''
+          echo "Durdraw development environment"
+          echo "Run 'durdraw' to start the application"
+          echo "Optional: ansilove (PNG export), neofetch (system info)"
+        '';
+      };
+    };
 }
