7516578447939a64da0453820a5da87103313a13
Travis Huffman <huffmantravis57@protonmail.com>
2025-07-09 09:45:20 -0500

    updating flake

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
       {
         packages = {
           default = self.packages.${system}.durdraw;
+          format = "setuptools";
           pyproject = true;
           
           durdraw = pkgs.python3Packages.buildPythonApplication rec {
